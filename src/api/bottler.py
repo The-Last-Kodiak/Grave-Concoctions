f44b98d4af3cecae96d19c1d9f4f211fbd1126b4
The-Last-Kodiak <theseventhkodiak@gmail.com>
2024-11-05 13:34:27 -0800

    tada

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -23,22 +23,26 @@ class PotionInventory(BaseModel):
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
     potion_totals = {'red': 0, 'green': 0, 'blue': 0, 'dark': 0}
     type_str = ['red', 'green', 'blue', 'dark']
-    
     for potinv in potions_delivered:
         potion_type = potinv.potion_type
         quantity = potinv.quantity
+        query = "SELECT sku FROM potions WHERE typ = :potion_type LIMIT 1"
+        with db.engine.begin() as connection:
+            result = connection.execute(sqlalchemy.text(query), {"potion_type": potion_type}).fetchone()
+        sku = result[0] if result else None
+        if not sku:
+            continue  # Skip if no matching SKU is found
         for i, amount in enumerate(potion_type):
             if amount > 0:
                 potion_totals[type_str[i]] += amount * quantity
-                update_potion_inventory(f"{type_str[i].upper()}_CONCOCTION", quantity)
-    
+                update_potion_inventory(sku, quantity)
     for color, total in potion_totals.items():
         if total > 0:
             update_ml(color.upper(), -total)
             print(f"Total {color} ml used: {total}")
-    
     print(f"CALLED BOTTLES DELIVERY. order_id: {order_id}")
     return {"success": True}
+
 
 
 @router.post("/plan")
